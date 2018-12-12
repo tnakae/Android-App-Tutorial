cbe30bdd2c7239936a2a61850033f88a271b90a8
Toshihiro Nakae <tnakae@users.noreply.github.com>
2018-12-12 10:20:00 +0000

    add Chap4

---
+++ Chap4/HelloSample/app/src/main/java/com/websarva/wings/android/hellosample/HelloSampleActivity.java
@@ -0,0 +1,13 @@
+package com.websarva.wings.android.hellosample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloSampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_sample);
+    }
+}

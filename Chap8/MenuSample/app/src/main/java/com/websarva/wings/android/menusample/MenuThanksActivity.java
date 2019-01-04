ccc490f859a5feceb7e32c3a833dbcf9ad7c699c
Toshihiro Nakae <tnakae@users.noreply.github.com>
2019-01-04 23:51:38 +0900

    Implement back button

--- Chap8/MenuSample/app/src/main/java/com/websarva/wings/android/menusample/MenuThanksActivity.java
+++ Chap8/MenuSample/app/src/main/java/com/websarva/wings/android/menusample/MenuThanksActivity.java
@@ -1,9 +1,10 @@
 package com.websarva.wings.android.menusample;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MenuThanksActivity extends AppCompatActivity {
@@ -25,12 +26,22 @@ public class MenuThanksActivity extends AppCompatActivity {
         // TextViewに定食名と金額を表示。
         tvMenuName.setText(menuName);
         tvMenuPrice.setText(menuPrice);
+
+        // アクションバーを取得
+        ActionBar actionBar = getSupportActionBar();
+        // アクションバーの [戻る] メニューを有効に設定
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
-    /**
-     * 戻るボタンをタップしたときの処理
-     */
-    public void onBackButtonClick(View view) {
-        finish();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // 選択されたメニューのIDを取得。
+        int itemId = item.getItemId();
+        // 選択されたメニューが [戻る] の場合、アクティビティを終了。
+        if (itemId == android.R.id.home) {
+            finish();
+        }
+        // 親クラスの同名メソッドを呼び出し、その戻り値を返却。
+        return super.onOptionsItemSelected(item);
     }
 }
